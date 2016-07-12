7208181896f966e4a13575fe46d3c819af2cbbf0
Phillip Gomez <gomez.phillip@gmail.com>
2016-07-12 12:04:14 -0700

    Add basic group composite support

--- src/binary_transformer/core.clj
+++ src/binary_transformer/core.clj
@@ -12,13 +12,15 @@
                                              #(<= % 0xFFFF)))))
 
 (def primitive-types #{:int32 :int16 :int8})
-(s/def ::rule (s/cat :name keyword? :count (s/? int?) :type primitive-types))
-(s/def ::binary-spec (s/coll-of ::rule))
+(s/def ::rule (s/cat :name keyword? :count (s/? int?) :type (s/or :primitive primitive-types
+                                                                  :group     ::binary-spec)))
+(s/def ::binary-spec (s/coll-of (s/or :rule  ::rule
+                                      :group ::binary-spec)))
 
 ;==============================================================================
 ; Conversion functions
 
-(defn ^:private ->bytes-array [bytes]
+(defn ^:private clj->bytes-array [bytes]
   (let [bytes (->> bytes
                    (s/conform ::bytes)
                    (apply hash-map))]
@@ -28,18 +30,42 @@
 ;==============================================================================
 ; Transformation functions
 
+(declare transform-composites)
+
 (defn extract-simple [f m rule]
   (assoc-in m [:res (:name rule)] (f (:data m))))
 
-(defmulti transform-rule #(:type %2))
-(defmethod transform-rule :int32 [m rule] (extract-simple #(.getInt %) m rule))
-(defmethod transform-rule :int16 [m rule] (extract-simple #(.getShort %) m rule))
-(defmethod transform-rule :int8 [m rule] (extract-simple #(.get %) m rule))
+(defmulti transform-primitive #(second (:type %2)))
+(defmethod transform-primitive :int32 [m rule] (extract-simple #(.getInt %) m rule))
+(defmethod transform-primitive :int16 [m rule] (extract-simple #(.getShort %) m rule))
+(defmethod transform-primitive :int8 [m rule] (extract-simple #(.get %) m rule))
 
-(defn transform
+(defmulti transform-rule #(first (:type %2)))
+(defmethod transform-rule :primitive [state rule]
+  (transform-primitive state rule))
+(defmethod transform-rule :group [state rule]
+  (assoc-in state [:res (:name rule)] (get (transform-composites state (:type rule)) :res)))
+
+(defmulti transform-composites #(first %2))
+(defmethod transform-composites :rule [state spec]
+  (transform-rule state (second spec)))
+(defmethod transform-composites :group [state spec]
+  (reduce transform-composites state (second spec)))
+
+
+(defn binary->clj
   [spec bytes]
   {:pre [(s/valid? ::binary-spec spec)
          (s/valid? ::bytes bytes)]}
-  (let [byte-buf (ByteBuffer/wrap (->bytes-array bytes))
+  (let [byte-buf (ByteBuffer/wrap (clj->bytes-array bytes))
         spec (s/conform ::binary-spec spec)]
-    (get (reduce transform-rule {:res {} :data byte-buf} spec) :res)))
+    (get (reduce transform-composites {:res {} :data byte-buf} spec) :res)))
+
+(comment
+
+  (def header [[:header1 :int16]
+               [:header2 :int16]])
+
+  (s/conform ::binary-spec [[:field1 :int32] [:header header] [:field2 :int32]])
+
+  )
--- test/binary_transformer/core_test.clj
+++ test/binary_transformer/core_test.clj
@@ -2,20 +2,34 @@
   (:require [clojure.test :refer :all]
             [binary-transformer.core :refer :all]))
 
-(deftest primitive-tests
+(deftest simple-tests
   (testing "Single integer"
-    (is (= {:field1 (unchecked-int 0xABCD1234)} (transform [[:field1 :int32]] [0xAB 0xCD 0x12 0x34]))))
+    (is (= {:field1 (unchecked-int 0xABCD1234)} (binary->clj [[:field1 :int32]] [0xAB 0xCD 0x12 0x34]))))
   (testing "Two integers"
     (is (= {:field1 (unchecked-int 0xABCD1234)
             :field2 (unchecked-int 0x34523454)}
-           (transform [[:field1 :int32]
+           (binary->clj [[:field1 :int32]
                        [:field2 :int32]]
                       [0xAB 0xCD 0x12 0x34, 0x34 0x52 0x34 0x54]))))
   (testing "Multiple primitive types"
     (is (= {:field1 (unchecked-int 0xABCD1234)
             :field2 (unchecked-byte 0xDC)
             :field3 (unchecked-short 0x3452)}
-           (transform [[:field1 :int32]
+           (binary->clj [[:field1 :int32]
                        [:field2 :int8]
                        [:field3 :int16]]
                       [0xAB 0xCD 0x12 0x34, 0xDC, 0x34 0x52 ])))))
+
+(deftest composite-tests
+  (let [header [[:field1 :int32] [:field2 :int16]]
+        header-res {:field1 (unchecked-int 0xABCD1234) :field2 (unchecked-short 0x7890)}
+        header-data [0xAB 0xCD 0x12 0x34, 0x78 0x90]]
+    (testing "Single composition"
+      (is (= header-res (binary->clj [header] header-data))))
+    (testing "Inline composition"
+      (is (= (merge header-res {:field3 (unchecked-int 0x98765432)})
+             (binary->clj [header [:field3 :int32]] (concat header-data [0x98 0x76 0x54 0x32])))))
+    (testing "Named group"
+      (is (= {:header header-res :field3 (unchecked-int 0x98765432)}
+             (binary->clj [[:header header] [:field3 :int32]] (concat header-data [0x98 0x76 0x54 0x32])))))))
+
