f67ac6a7dc84c461132ff23bdc96a1476c561cd0
Phillip Gomez <gomez.phillip@gmail.com>
2016-07-12 15:48:58 -0700

    Add encode functionality

--- src/binary_transformer/core.clj
+++ src/binary_transformer/core.clj
@@ -20,7 +20,7 @@
 ;==============================================================================
 ; Conversion functions
 
-(defn ^:private clj->bytes-array [bytes]
+(defn clj->bytes-array [bytes]
   (let [bytes (->> bytes
                    (s/conform ::bytes)
                    (apply hash-map))]
@@ -28,30 +28,29 @@
       (into-array Byte/TYPE (map unchecked-byte (:num-sequence bytes))))))
 
 ;==============================================================================
-; Transformation functions
+; Decode functions
 
-(declare transform-composites)
+(declare decode-composites)
 
-(defn extract-simple [f m rule]
-  (assoc-in m [:res (:name rule)] (f (:data m))))
+(defmulti  decode-primitive (fn [type _] type))
+(defmethod decode-primitive :int32 [_ byte-buf] (.getInt byte-buf))
+(defmethod decode-primitive :int16 [_ byte-buf] (.getShort byte-buf))
+(defmethod decode-primitive :int8  [_ byte-buf] (.get byte-buf))
 
-(defmulti transform-primitive #(second (:type %2)))
-(defmethod transform-primitive :int32 [m rule] (extract-simple #(.getInt %) m rule))
-(defmethod transform-primitive :int16 [m rule] (extract-simple #(.getShort %) m rule))
-(defmethod transform-primitive :int8 [m rule] (extract-simple #(.get %) m rule))
+(defmulti decode-rule #(first (:type %2)))
+(defmethod decode-rule :primitive [state rule]
+  (let [byte-buf (:byte-buf state)
+        field (:name rule)
+        type (second (:type rule))]
+    (assoc-in state [:res field] (decode-primitive type byte-buf))))
+(defmethod decode-rule :group [state rule]
+  (assoc-in state [:res (:name rule)] (get (decode-composites state (:type rule)) :res)))
 
-(defmulti transform-rule #(first (:type %2)))
-(defmethod transform-rule :primitive [state rule]
-  (transform-primitive state rule))
-(defmethod transform-rule :group [state rule]
-  (assoc-in state [:res (:name rule)] (get (transform-composites state (:type rule)) :res)))
-
-(defmulti transform-composites #(first %2))
-(defmethod transform-composites :rule [state spec]
-  (transform-rule state (second spec)))
-(defmethod transform-composites :group [state spec]
-  (reduce transform-composites state (second spec)))
-
+(defmulti decode-composites #(first %2))
+(defmethod decode-composites :rule [state spec]
+  (decode-rule state (second spec)))
+(defmethod decode-composites :group [state spec]
+  (reduce decode-composites state (second spec)))
 
 (defn binary->clj
   [spec bytes]
@@ -59,13 +58,73 @@
          (s/valid? ::bytes bytes)]}
   (let [byte-buf (ByteBuffer/wrap (clj->bytes-array bytes))
         spec (s/conform ::binary-spec spec)]
-    (get (reduce transform-composites {:res {} :data byte-buf} spec) :res)))
+    (get (reduce decode-composites {:res {} :byte-buf byte-buf} spec) :res)))
+
+
+;==============================================================================
+; Encode functions
+
+(declare encode-composites)
+(declare encode-composites-size)
+
+(defmulti  encode-primitive! (fn [type _ _ _] type))
+(defmethod encode-primitive! :int32 [_ field byte-buf data] (.putInt   byte-buf (get data field)))
+(defmethod encode-primitive! :int16 [_ field byte-buf data] (.putShort byte-buf (get data field)))
+(defmethod encode-primitive! :int8  [_ field byte-buf data] (.put      byte-buf (get data field)))
+
+(defmulti encode-rule #(first (:type %2)))
+(defmethod encode-rule :primitive [state rule]
+  (let [{:keys [byte-buf data]} state
+        type (second (:type rule))
+        field (:name rule)]
+    (encode-primitive! type field byte-buf data)
+    state))
+(defmethod encode-rule :group [state rule]
+  (let [data (:data state)
+        scoped-data (get data (:name rule))]
+    (encode-composites (assoc state :data scoped-data) (:type rule)))
+  state)
+
+(defmulti encode-composites #(first %2))
+(defmethod encode-composites :rule [state spec]
+  (encode-rule state (second spec)))
+(defmethod encode-composites :group [state spec]
+  (reduce encode-composites state (second spec)))
+
+
+(def primitive-size {:int32 4 :int16 2 :int8 1})
+
+(defmulti encode-rule-size #(first (:type %2)))
+(defmethod encode-rule-size :primitive [state rule]
+  (update state :res + (get primitive-size (second (:type rule)))))
+(defmethod encode-rule-size :group [state rule]
+  (update state :res + (get (encode-composites-size (assoc {} :res 0 :data (get (:data state) (:name rule))) (:type rule)) :res)))
+
+(defmulti encode-composites-size #(first %2))
+(defmethod encode-composites-size :rule [state spec]
+  (encode-rule-size state (second spec)))
+(defmethod encode-composites-size :group [state spec]
+  (reduce encode-composites-size state (second spec)))
+
+(defn encode-size [spec m]
+  (get (reduce encode-composites-size {:res 0 :data m} spec) :res))
+
+(defn clj->binary
+  [spec m]
+  {:pre [(s/valid? ::binary-spec spec)]}
+  (let [spec (s/conform ::binary-spec spec)
+        size (encode-size spec m)
+        byte-buf (ByteBuffer/allocate size)]
+    (.array (get (reduce encode-composites {:byte-buf byte-buf :data m} spec) :byte-buf))))
 
 (comment
 
   (def header [[:header1 :int16]
                [:header2 :int16]])
+  (def group1 [[group]])
 
-  (s/conform ::binary-spec [[:field1 :int32] [:header header] [:field2 :int32]])
+  (s/conform ::binary-spec [[[[:field1 :int32]]] [:header header] [:field2 :int32]])
+
+  (encode-size (s/conform ::binary-spec [[:field1 :int32]]) {:field1 (unchecked-int 0xABCD1234)})
 
   )
--- test/binary_transformer/core_test.clj
+++ test/binary_transformer/core_test.clj
@@ -33,3 +33,22 @@
       (is (= {:header header-res :field3 (unchecked-int 0x98765432)}
              (binary->clj [[:header header] [:field3 :int32]] (concat header-data [0x98 0x76 0x54 0x32])))))))
 
+(deftest encode-simple-tests
+  (testing "Single primitives"
+    (is (= (vec (clj->bytes-array [0xAB 0xCD 0x12 0x34]))
+           (vec (clj->binary [[:field1 :int32]] {:field1 (unchecked-int 0xABCD1234)}))))))
+
+
+(deftest encode-composites-tests
+  (let [header [[:field1 :int32] [:field2 :int16]]
+        header-data {:field1 (unchecked-int 0xABCD1234) :field2 (unchecked-short 0x7890)}
+        header-res (vec (clj->bytes-array [0xAB 0xCD 0x12 0x34, 0x78 0x90]))]
+    (testing "Single composition"
+      (is (= header-res
+             (vec (clj->binary [header] header-data)))))
+    (testing "Inline composition"
+      (is (= (concat header-res (vec (clj->bytes-array [0x54 0x34])))
+             (vec (clj->binary [header [:field3 :int16]] (merge header-data {:field3 (unchecked-short 0x5434)}))))))
+    (testing "Named group"
+      (is (= (concat header-res (vec (clj->bytes-array [0x54 0x34])))
+             (vec (clj->binary [[:header header] [:field3 :int16]] {:header header-data :field3 (unchecked-short 0x5434)})))))))
