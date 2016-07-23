b208de2fc64dcaf7088652e00d41f485c455be92
Phillip Gomez <gomez.phillip@gmail.com>
2016-07-23 13:51:18 -0700

    Add array type

--- src/binary_transformer/core.clj
+++ src/binary_transformer/core.clj
@@ -17,16 +17,21 @@
 (defmulti entry-args-spec :type)
 (defmethod entry-args-spec :group [_] ::binary-spec)
 (s/def ::size (s/or :const-size int? :path-to-size (s/coll-of keyword?)))
-(defmethod entry-args-spec :array [_] (s/cat :spec ::binary-spec
+(defmethod entry-args-spec :array [_] (s/cat :item-type (s/or :spec ::binary-spec
+                                                              :type keyword?)
                                              :options (s/keys* :req-un [::size])))
 (defmethod entry-args-spec :default [_] (s/* ::s/any))
 
+(defn keyword-or-int? [x]
+  (or (keyword? x)
+      (int? x)))
+
 ;; Note that args are conformed later in the process
-(s/def ::entry (s/cat :name keyword?
+(s/def ::entry (s/cat :name keyword-or-int?
                       :type keyword?
                       :args (s/* ::s/any)))
 
-(s/def ::binary-spec (s/coll-of (s/or :entry        ::entry
+(s/def ::binary-spec (s/* (s/or :entry        ::entry
                                       :nested-spec  ::binary-spec)))
 
 ;==============================================================================
@@ -44,7 +49,6 @@
 ; Encode/Decode functions
 
 (defn reduce-spec [m spec f]
-  (println spec)
   (let [m (if (:spec m) m (assoc m :spec spec))]
     (reduce (fn [m spec-entry]
               (let [spec-entry (apply hash-map spec-entry)]
@@ -72,6 +76,12 @@
         value (.getShort bits)]
     (assoc-in m path value)))
 
+(defmethod decode-type :int8 [m]
+  (let [{:keys [entry path bits]} m
+        path (conj path (:name entry))
+        value (.get bits)]
+    (assoc-in m path value)))
+
 (defmethod decode-type :group [m]
   (let [{:keys [entry path]} m
         group-path (conj path (:name entry))
@@ -81,20 +91,27 @@
         (reduce-spec group-spec decode-type)
         (assoc :path path :entry entry))))
 
-(defmethod decode-type :array-item [m]
-  (let [args (get-in m [:entry :args])
-        {:keys [const-size path-to-size]}  (apply hash-map (get-in args [:options :size]))
-        size (or const-size
-                 (get-in m (concat [:result] path-to-size)))
-        spec (repeat size (:spec args))]))
-
 (defmethod decode-type :array [m]
-  (let [args (get-in m [:entry :args])
-        {:keys [const-size path-to-size]}  (apply hash-map (get-in args [:options :size]))
+  (let [{:keys [entry path]} m
+        args (:args entry)
+        {:keys [const-size path-to-size]} (apply hash-map (get-in args [:options :size]))
         size (or const-size
                  (get-in m (concat [:result] path-to-size)))
-        spec (repeat size (:spec args))]
-    (reduce-spec m spec decode-type)))
+        item-type (apply hash-map (:item-type args))
+        spec (->> (range size)
+                  (map (fn [i]
+                         (cond
+                           (:spec item-type) [i :group (:spec item-type)]
+                           (:type item-type) [i (:type item-type)])))
+                  ;(sort-by first)
+                  (s/conform ::binary-spec))
+        array-path (conj path (:name entry))]
+    (-> m
+        (assoc :path array-path)
+        (assoc-in array-path (vec (repeat size nil)))
+        (reduce-spec spec decode-type)
+        (assoc :path path))))
+
 
 (defn decode [spec byte-buf]
   (let [spec (s/conform ::binary-spec spec)
@@ -104,74 +121,15 @@
         (reduce-spec spec decode-type)
         :result)))
 
-
 (comment
 
-
-  (def header [[:id       :int32]
-               [:type     :group [[:inner1 :int16]
-                                  [:inner2 :int16]]]
-               [:otherttd :int32]
-               [:size     :int32]])
-
-  (def item [[:a :int16]])
+  (def header [[:sync :int8]
+               [:size :int8]])
 
   (def message [[:header :group header]
-                [:items  :array item :size [:header :size]]])
+                [:items :array :int8 :size 5]])
 
-
- (decode message [0x12 0x34 0x56 0x78, 0x00 0x00, 0x00 0x20, 0x34 0x56 0x78 0x90, 0x00 0x00 0x00 0x03, 0x00 0x01, 0x00 0x10, 0x00 0x20])
-
-  (s/conform ::binary-spec message )
-  (s/explain ::binary-spec [[:item :group item]])
-
-
-  (def array-args [[[:a :int32]] :size [:header :size]])
-  (s/conform (entry-args-spec {:type :array}) array-args)
-  (s/explain (entry-args-spec {:type :array}) array-args)
-  ;[:id :type :ref? :option-hash]
-
-  ;(def my-choices {1 choice1
-  ;                 2 choice2})
-
-  ;(def reasons {1 reason1
-  ;              2 reason2})
-
-  ;[[:sync   :int32     :value 0x34 :nbits 2]
-  ; [:header :group     header   :size 1]
-  ; [:items  :array     item     :size [:header :size]]
-  ; [:body   :choice-of commands :using [:header :type]]
-  ; [:reason :enum32    reasons]
-  ; [:number :scalar    :scale 1.4565 :offset 2]]
-
-
-
-  ;encode-preprocess :type => {:size :data :path :entry :spec}
-  ;decode-preprocess :type => {:data :path :entry :spec}
-
-  ;encode :type => {:bytes :data :path :entry :spec}
-  ;decode :type => {:bits :result :path :entry :spec}
-
-  ;(def message [[:header :group spe]
-  ;              []])
-
-  ;(vec
-  ;  (clj->binary message {:header {:id 0x12345678
-  ;                                 :type 0x1ABC
-  ;                                 :other-id 0x1987
-  ;                                 :size 5}
-  ;                        :items [0x12345678 0x13345678 0x14345678 0x15345678 0x16345678]}))
-
-
-
-
-
-  (def items [{:a 0x12345678 :b 0x1987}
-              {:a 0x13345678 :b 0x1887}
-              {:a 0x14345678 :b 0x1787}
-              {:a 0x15345678 :b 0x1687}
-              {:a 0x16345678 :b 0x1587}])
-
+  (decode message [0xAB, 0x0A, 0x09 0x08 0x07 0x06 0x05 0x04 0x03 0x02 0x01 0x00])
 
 
   )
