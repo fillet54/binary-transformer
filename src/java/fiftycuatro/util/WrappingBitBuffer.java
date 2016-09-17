1432ef2db4b13beb056bc7ba3e10d16030f683f3
Phillip Gomez <gomez.phillip@gmail.com>
2016-09-17 12:40:18 -0700

    Add array and group support

--- src/java/fiftycuatro/util/WrappingBitBuffer.java
+++ src/java/fiftycuatro/util/WrappingBitBuffer.java
@@ -5,14 +5,24 @@ import java.nio.ByteOrder;
 
 public class WrappingBitBuffer implements BitBuffer {
 
+    private final ByteBuffer byteBuffer;
     private ByteOrder order = ByteOrder.BIG_ENDIAN;
+    private byte remainingBits = 0x0;
+    private int remainingBitCount = 0;
+
+    private final static byte[] masks = { 0x00, 0x01, 0x03, 0x07, 0x0F, 0x1F, 0x3F, 0x7F, (byte)0xFF};
+    private final static byte[] signMasks = { (byte)0x80, 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40};
+    private final static byte[] twosCompMasks = { 0x00, (byte)0xFE, (byte)0xFC, (byte)0xF8, (byte)0xF0, (byte)0xE0, (byte)0xC0, (byte)0x80 };
 
     public static BitBuffer wrap(byte[] bytes) {
        return wrap(ByteBuffer.wrap(bytes));
     }
-
     public static BitBuffer wrap(ByteBuffer byteBuffer) {
         return new WrappingBitBuffer(byteBuffer);
+    }
+
+    protected WrappingBitBuffer(ByteBuffer byteBuffer) {
+        this.byteBuffer = byteBuffer;
     }
 
     public BitBuffer order(ByteOrder order) {
@@ -20,22 +30,15 @@ public class WrappingBitBuffer implements BitBuffer {
         return this;
     }
 
-    private final ByteBuffer byteBuffer;
-    private byte remainingBits = 0x0;
-    private int remainingBitCount = 0;
-
-    protected WrappingBitBuffer(ByteBuffer byteBuffer) {
-       this.byteBuffer = byteBuffer;
-    }
-
-    private final static byte[] masks = { 0x00, 0x01, 0x03, 0x07, 0x0F, 0x1F, 0x3F, 0x7F, (byte)0xFF};
-
     private byte getByteInShort(Short s, int bitsInShort, int bitsToGet) {
         int shift = (bitsInShort > 8 ? 16 : 8) - (8 - bitsInShort) - bitsToGet;
         int mask = masks[bitsToGet];
         return (byte)((s >> shift) & mask);
     }
 
+    /**
+     * Main building block for this class.
+     */
     @Override
     public byte getByte(int nBits) {
         short s;
@@ -54,28 +57,55 @@ public class WrappingBitBuffer implements BitBuffer {
         return returnByte;
     }
 
+    /** Helper function for getting an array of bytes with the MSB
+     *  correctly zeroized depending on the number of bits and
+     *  endianess.
+     *
+     * nBytes is how large of a byte array should returned with
+     * Most significant bytes/bits filled with 0. This is useful
+     * when you know or care about what type you are decoding.
+     *
+     * Endianess may not be fully thought out but bit endianess
+     * is always Big.
+     */
+    private byte[] getBytes(int nBytes, int nBits, boolean isSigned, ByteOrder order) {
+        byte[] bytes = new byte[nBytes];
+        int bytesForBits = ((nBits -1) / 8) + 1;
+        int startIndex = order == ByteOrder.BIG_ENDIAN ? 4 - bytesForBits : 0;
+
+        for(int bitsLeft = nBits, i = startIndex; bitsLeft > 0; bitsLeft -= 8, i++) {
+            int bitsToGet = bitsLeft % 8;
+            bytes[i] = getByte(bitsToGet == 0 ? 8 : bitsToGet);
+        }
+
+        // Do conversion for signed numbers
+        if (isSigned) {
+            int msbIndex = order == ByteOrder.BIG_ENDIAN ? startIndex : bytesForBits - 1;
+
+            // Only have to worry about items that have
+            // most significant bit set
+            if ((bytes[msbIndex] & signMasks[nBits % 8]) == 1) {
+                bytes[msbIndex] = (byte)(bytes[msbIndex] | twosCompMasks[nBits % 8]);
+                if ((bytes[msbIndex] & signMasks[nBits % 8]) == 0x1) {
+                    int start = order == ByteOrder.BIG_ENDIAN ? 0 : bytesForBits;
+                    int end = order == ByteOrder.BIG_ENDIAN ? startIndex : nBytes;
+                    for (int i = start; i < end; i++) {
+                        bytes[i] = (byte) 0xFF;
+                    }
+                }
+            }
+        }
+        return bytes;
+    }
+
     @Override
     public int getInt(int nBits) {
         return getInt(nBits, this.order);
     }
 
-    private final static byte[] signMasks = { (byte)0x80, 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40};
-    private final static byte[] twosCompMasks = { 0x7F, 0x00, 0x01, 0x03, 0x07, 0x0F, 0x1F, 0x3F};
     @Override
     public int getInt(int nBits, ByteOrder order) {
-        byte[] bytes = getBytes(4, nBits, order);
-        int bytesUsed = ((nBits - 1) / 8) + 1;
-        int msbIdx = order == ByteOrder.BIG_ENDIAN ? 4 - bytesUsed : bytesUsed - 1;
-        int bitloc = signMasks[(nBits % 8)];
-        int sign = (bytes[msbIdx] & bitloc) == 0 ? 1 : -1;
-
-        if (sign == -1) {
-            // twos complement convert
-            bytes[msbIdx] = (byte)((~bytes[msbIdx] & twosCompMasks[nBits % 8]) + 1);
-        }
-
-        return sign
-                * ByteBuffer.wrap(bytes)
+        return ByteBuffer.wrap(getBytes(4, nBits, true, order))
                 .order(order)
                 .getInt();
     }
@@ -87,7 +117,7 @@ public class WrappingBitBuffer implements BitBuffer {
 
     @Override
     public long getUnsignedInt(int nBits, ByteOrder order) {
-        return ByteBuffer.wrap(getBytes(4, nBits, order))
+        return ByteBuffer.wrap(getBytes(4, nBits, false, order))
                 .order(order)
                 .getInt() & 0xFFFFFFFFl;
     }
@@ -99,24 +129,10 @@ public class WrappingBitBuffer implements BitBuffer {
 
     @Override
     public long getLong(int nBits, ByteOrder order) {
-        return ByteBuffer.wrap(getBytes(8, nBits, order))
+        byte[] bytes = getBytes(8, nBits, true, order);
+        return ByteBuffer.wrap(getBytes(8, nBits, true, order))
                 .order(order)
                 .getLong();
-    }
-
-    // nBytes is how large of a byte array should be filled with 0
-    // Endianess may not be fully thought out.
-    // Bit endianess is always Big.
-    private byte[] getBytes(int nBytes, int nBits, ByteOrder order) {
-        byte[] bytes = new byte[nBytes];
-        int bytesForBits = ((nBits -1) / 8) + 1;
-        int idx = order == ByteOrder.BIG_ENDIAN ? 4 - bytesForBits : 0;
-
-        for(int bitsLeft = nBits, i = idx; bitsLeft > 0; bitsLeft -= 8, i++) {
-            int bitsToGet = bitsLeft % 8;
-            bytes[i] = getByte(bitsToGet == 0 ? 8 : bitsToGet);
-        }
-        return bytes;
     }
 
 
@@ -126,10 +142,13 @@ public class WrappingBitBuffer implements BitBuffer {
         byteBuffer.rewind();
     }
 
-    private final String toStringFormat = "%s[pos=%d lim=%d cap=%d rem=%d cnt=%d]";
-
     public String toString() {
-        return String.format(toStringFormat, WrappingBitBuffer.class.getCanonicalName(),
-                byteBuffer.position(), byteBuffer.limit(), byteBuffer.capacity(), remainingBits, remainingBitCount);
+        return String.format("%s[pos=%d lim=%d cap=%d rem=%d cnt=%d]",
+                WrappingBitBuffer.class.getCanonicalName(),
+                byteBuffer.position(),
+                byteBuffer.limit(),
+                byteBuffer.capacity(),
+                remainingBits,
+                remainingBitCount);
     }
 }
